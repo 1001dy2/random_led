b4a6c7b7d80d87c35634584fb614a66a61d88c54
1001dy2 <115143514+1001dy2@users.noreply.github.com>
2022-12-08 16:47:19 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
+let list: number[] = []
+let n = 0
+let _ = 0
+let a = 0
+let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    list = []
     for (let index = 0; index < 25; index++) {
-    	
+        list.push(0)
+    }
+    n = 0
+    while (n < _) {
+        a = randint(0, 24)
+        if (list[a] == 0) {
+            list[a] = 1
+            n += 1
+            light2(a + 1)
+        }
     }
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    _ = _ + 1
 })
+function light2 (num: number) {
+    if (num % 5 == 0) {
+        x = 4
+        y = Math.floor(num / 5) - 0
+    } else {
+        x = num % 5 - 1
+        y = Math.floor(num / 5)
+    }
+    led.plot(x, y)
+}
